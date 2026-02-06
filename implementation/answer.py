a5a937632f3859b4a5eee2f8532725ef4cf0d809
szymonMCS <szymon.maciejewski.programista@gmail.com>
2026-02-06 17:36:45 +0100

    added simplke gradio frontend and refactor remaining functions for better work

--- implementation/answer.py
+++ implementation/answer.py
@@ -3,36 +3,14 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from openai import OpenAI
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage, convert_to_messages
+from langchain_core.documents import Document
 
 load_dotenv(override=True)
 
 DB_NAME="vector_db"
 MODEL = "gpt-4.1-nano"
+RETRIEVAL_K = 10
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-
-def setup_retriever(db_name, embeddings, k = 10):
-    """
-    konfiguracja narzędzia do wyszukiwania podobnych dokumentów
-
-    Args:
-        db_name: Ścieżka do folderu z bazą (np. "vector_db")
-        embeddings: Obiekt embeddingów (muszą być te same co przy tworzeniu!)
-        k: Ile dokumentów zwrócić (domyślnie 10)
-    
-    Returns:
-        Retriever: Obiekt który można wywołać z pytaniem
-    """
-    vectorstore = Chroma(
-        persist_directory = db_name,
-        embedding_function = embeddings
-    )
-    retriever = vectorstore.as_retriever(search_kwargs = {"k": k})
-    return retriever
-
-retriever = setup_retriever(DB_NAME, embeddings, k=10)
-#docs = retriever.invoke("Jak nazywa się sklep?")
-#print(docs[0].page_content)
-#print(docs[0].metadata) 
 
 SYSTEM_PROMPT = """
 Jesteś kompetentnym i przyjaznym asystentem reprezentującym sklep z elektroniką NeoGadżet.
@@ -45,39 +23,40 @@ Dla kontekstu, oto konkretne fragmenty z Bazy Wiedzy, które mogą być bezpośr
 Biorąc pod uwagę ten kontekst, odpowiedz na pytanie użytkownika. Bądź dokładny, merytoryczny i wyczerpujący.
 """
 
-def answer_question(question, history = [], retriever = None, llm = None):
+vectorstore = Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+retriever = vectorstore.as_retriever()
+llm = ChatOpenAI(temperature=0, model_name=MODEL)
+
+def fetch_context(question: str) -> list[Document]:
+    """
+    Pobiera odpowiednie dokumenty kontekstowe do pytania
+    """
+    return retriever.invoke(question, k=RETRIEVAL_K) 
+
+def combined_question(question: str, history: list[dict] = []) -> str:
+    """
+    Łączy odpowiedzi użytkownika w jeden string
+    """
+    contents = []
+    for m in history:
+        if m["role"] == "user":
+            content = m["content"]
+            if isinstance(content, list):
+                content = " ".join(str(x) for x in content)
+            contents.append(content)
+    prior = "\n".join(contents)
+    return prior + "\n" + question
+
+def answer_question(question: str, history: list[dict] = []) -> tuple[str, list[Document]]:
     """
     Funkcja RAG pobierająca kontekst i generująca odpowiedź
-
-    Args:
-        question: Pytanie użytkownika
-        history: Lista poprzednich wiadomości (opcjonalnie)
-        retriever: Skonfigurowany retriever do wyszukiwania
-        llm: Model językowy
-    
-    Returns:
-        tuple: (odpowiedź, lista_dokumentów)
-               Odpowiedź to string, lista_dokumentów to chunki użyte do odpowiedzi
     """
-    docs = retriever.invoke(question)
-    print(f"Znaleziono {len(docs)} dokumentów dla pytania: '{question}'")
-
+    combined = combined_question(question, history)
+    docs = fetch_context(combined)
     context = "\n\n".join(doc.page_content for doc in docs)
-    print(f"Całkowita długość kontekstu: {len(context)} znaków")
-
-    system_prompt = SYSTEM_PROMPT.format(context = context)
-    messages = []
-    messages.append(SystemMessage(content = system_prompt))
-    if history:
-        messages.extend(convert_to_messages(history))
-    messages.append(HumanMessage(content = question))
-    print("Generowanie odpowiedzi")
+    system_prompt = SYSTEM_PROMPT.format(context=context)
+    messages = [SystemMessage(content=system_prompt)]
+    messages.extend(convert_to_messages(history))
+    messages.append(HumanMessage(content=question))
     response = llm.invoke(messages)
     return response.content, docs
-
-#odpowiedz, uzyte_dokumenty = answer_question(
-#    "Co to NeoGadżet?",
-#    retriever=retriever,
-#    llm=ChatOpenAI(model = MODEL)
-#)
-#print(odpowiedz)
